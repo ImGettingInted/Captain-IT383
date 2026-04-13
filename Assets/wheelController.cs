4ab9b797dc671bd62f1176f12280bd646337ea59
ImGettingInted <rbg22@njit.edu>
2026-04-13 15:30:50 -0400

    PLEASE.

--- Assets/wheelController.cs
+++ Assets/wheelController.cs
@@ -9,14 +9,19 @@ public class WheelController : MonoBehaviour
     public GameObject rightHand;
     private Transform rightHandOriginalParent;
     private bool rightHandOnWheel = false;
-    private bool rightTriggerPressed  = false;
+    [SerializeField]
+    private InputActionReference rightTrigger;
+    private bool rightTriggerPressed = false;
     
     
     public GameObject leftHand;
     private Transform leftHandOriginalParent;
     private bool leftHandOnWheel = false;
+    [SerializeField]
+    private InputActionReference leftTrigger;
     private bool leftTriggerPressed = false;
-
+    
+    
     public Transform[] snapPositions;
 
     public Transform forward;
@@ -30,25 +35,36 @@ public class WheelController : MonoBehaviour
 
     public Transform directionalObject;
 
-    public void OnLeftTriggerPressed()
+    private void OnEnable()
     {
-        leftTriggerPressed = true;
+        // Subscribe to events
+        rightTrigger.action.performed += OnRightTriggerPressed;
+        rightTrigger.action.canceled += OnRightTriggerReleased;
+
+        leftTrigger.action.performed += OnLeftTriggerPressed;
+        leftTrigger.action.canceled += OnLeftTriggerReleased;
+
+        // Must enable the actions to receive input
+        rightTrigger.action.Enable();
+        leftTrigger.action.Enable();
     }
 
-    public void OnRightTriggerPressed()
+    private void OnDisable()
     {
-        rightTriggerPressed = true;
+        // Unsubscribe to prevent memory leaks or errors when object is destroyed
+        rightTrigger.action.performed -= OnRightTriggerPressed;
+        rightTrigger.action.canceled -= OnRightTriggerReleased;
+
+        leftTrigger.action.performed -= OnLeftTriggerPressed;
+        leftTrigger.action.canceled -= OnLeftTriggerReleased;
     }
 
-    public void OnLeftTriggerReleased()
-    {
-        leftTriggerPressed = false;
-    }
+    // Context contains info about the input, like the value or phase
+    private void OnRightTriggerPressed(InputAction.CallbackContext context) => rightTriggerPressed = true;
+    private void OnRightTriggerReleased(InputAction.CallbackContext context) => rightTriggerPressed = false;
 
-    public void OnRightTriggerReleased()
-    {
-        rightTriggerPressed = false;
-    }
+    private void OnLeftTriggerPressed(InputAction.CallbackContext context) => leftTriggerPressed = true;
+    private void OnLeftTriggerReleased(InputAction.CallbackContext context) => leftTriggerPressed = false;
     
     public void Start()
     {
