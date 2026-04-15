cc5ce9dd810a700456e2d59b528b62fa711e657f
Noah Gibb <njg44@njit.edu>
2026-04-15 13:31:26 -0400

    Revert "reload feature"

--- Assets/Gun/FireBullet.cs
+++ Assets/Gun/FireBullet.cs
@@ -3,70 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using UnityEngine.InputSystem;
 
 public class FireBullet : MonoBehaviour
 {
-    public GameObject bullet;
+     public GameObject bullet;
     public Transform spawnPoint;
     public float bulletSpeed = 20f;
 
-    public int maxAmmo = 1;
-    private int currentAmmo;
-
-    public float reloadTime = 2f;
-    private bool isReloading = false;
-
-    public InputActionProperty reloadAction;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentAmmo = maxAmmo;
-
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
         grabbable.activated.AddListener(Fire);
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (reloadAction.action.WasPressedThisFrame() && !isReloading)
-        {
-            StartCoroutine(Reload());
-        }
+        
     }
 
     public void Fire(ActivateEventArgs args)
     {
-        if (isReloading) return;
-
-        if (currentAmmo <= 0)
-        {
-            Debug.Log("Out of ammo! Press B to reload.");
-            return;
-        }
-
         GameObject spawnBullet = Instantiate(bullet);
         spawnBullet.transform.position = spawnPoint.position;
-
-        Rigidbody rb = spawnBullet.GetComponent<Rigidbody>();
-        rb.linearVelocity = spawnPoint.forward * bulletSpeed;
-
+        spawnBullet.GetComponent<Rigidbody>().linearVelocity = spawnPoint.forward * bulletSpeed;
         Destroy(spawnBullet, 5f);
-
-        currentAmmo--;
-        Debug.Log("Ammo: " + currentAmmo);
     }
 
-    IEnumerator Reload()
-    {
-        isReloading = true;
-        Debug.Log("Reloading...");
-
-        yield return new WaitForSeconds(reloadTime);
-
-        currentAmmo = maxAmmo;
-        isReloading = false;
-
-        Debug.Log("Reloaded!");
-    }
 }
