510dd3d19035e84bfc8e1083d9d14194f95249bf
Kaylee-R-H <krh37@njit.edu>
2026-05-06 02:10:32 -0400

    game over for rocks

--- Assets/RocksScript.cs
+++ Assets/RocksScript.cs
@@ -1,10 +1,17 @@
-using System;
 using UnityEngine;
 
 public class RocksScript : MonoBehaviour
 {
-   public void OnTriggerEnter(Collider other)
-   {
-      Debug.Log("Crashed into rocks. Game Over.");
-   }
+    public float crashDamage = 999f;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth player = other.GetComponent<PlayerHealth>();
+
+        if (player != null)
+        {
+            Debug.Log("Crashed into rocks. Game Over.");
+            player.TakeDamage(crashDamage);
+        }
+    }
 }
